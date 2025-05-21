ae82b48ef93639d9f60363d3c043cd7da5f9fe9a
Oke Akoro <oke_akoro@hotmail.com>
2025-05-21 12:18:09 +0100

    updates

---
+++ Test/testscript.js
@@ -0,0 +1,10 @@
+  
+
+ function salute(){
+           var getName = prompt("What is your name?")
+            alert("Welcome to my page "+getName)
+        }
+
+salute()
+
+document.querySelector("h1").innerHTML = "Enjoy your stay "
